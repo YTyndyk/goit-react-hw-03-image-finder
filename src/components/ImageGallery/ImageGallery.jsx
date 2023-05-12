396046b3ad371cd9fe4a4f572199467a2cf643a7
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-05-12 17:46:15 +0300

    fixed mistake

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -32,7 +32,7 @@ class ImageGallery extends Component {
   }
 
   render() {
-    const { data, isLoading, error } = this.state;
+    const { data, isLoading } = this.state;
 
     return (
       <>
